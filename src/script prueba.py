e8827ca13d97317a6ace545f5c168fb7a169c6f2
hectorzzaa <hectorzzaa@gmail.com>
2025-11-16 18:31:33 +0100

    primera muestra, subida parcial

---
+++ src/script prueba.py
@@ -0,0 +1,42 @@
+import logging
+import os
+import findspark
+
+# Crear la carpeta "logs" si no existe
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)
+
+# Configurar logging
+logging.basicConfig(
+    filename=os.path.join(log_dir, "app.log"),
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+# Ejemplo de uso
+logging.info("La aplicación ha iniciado")
+logging.warning("Esto es una advertencia")
+logging.error("Esto es un error")
+
+
+try:
+    findspark.init()
+
+    from pyspark.sql import SparkSession
+    spark = SparkSession.builder \
+        .master("local[*]") \
+        .appName("ejercicio3") \
+        .getOrCreate()
+    print("✅ ¡Clúster de Spark virtual iniciado con éxito!")
+
+    print(f"Versión de Spark: {spark.version}")
+
+except Exception as e:
+    logging.error(f"Error al iniciar Spark: {e}")
+    print("❌ No se pudo iniciar el clúster de Spark virtual. Revisa los logs para más detalles.")
+
+from kaggle.api.kaggle_api_extended import KaggleApi
+
+os.environ['KAGGLE_CONFIG_DIR'] = r"C:\Users\pimie\Desktop\Desarrollo\TfgSpotify\Proyecto_de_big_data_e_inteligencia_artificial\environment"
+api = KaggleApi()
+api.authenticate()
