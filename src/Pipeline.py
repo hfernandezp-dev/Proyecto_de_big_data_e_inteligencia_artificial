88c0b1eb0205d4ecdf5cb0bcc736b2a688b52d34
hfernandezdev <hectorzzaa@gmail.com>
2025-11-17 16:54:50 +0100

    arreglo temporal

--- src/Pipeline.py
+++ src/Pipeline.py
@@ -2,36 +2,54 @@ import logging
 import os
 import findspark
 
-# Crear la carpeta "logs" si no existe
-log_dir = "logs"
-os.makedirs(log_dir, exist_ok=True)
 
-# Configurar logging
-logging.basicConfig(
-    filename=os.path.join(log_dir, "app.log"),
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s"
-)
+
+
+def iniciar_logger():
+
+    log_dir = "logs"
+    os.makedirs(log_dir, exist_ok=True)
+
+
+    logging.basicConfig(
+        filename=os.path.join(log_dir, "Pipeline.log"),
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s"
+    )
+
+
+def iniciar_spark():
+    findspark.init()
+    try:
+        from pyspark.sql import SparkSession
+        spark = SparkSession.builder \
+            .master("local[*]") \
+            .appName("ejercicio3") \
+            .getOrCreate()
+        logging.info(f"¡Clúster de Spark virtual iniciado con éxito! con la version {spark.version}")
+
+    except Exception as e:
+        logging.error(f"Error al iniciar Spark: {e}")
+        print("❌ No se pudo iniciar el clúster de Spark virtual. Revisa los logs para más detalles.")
+
+
+def limpieza_datos():
+
+
+
+
+
+
+def main():
+    iniciar_logger()
+    iniciar_spark()
+
+
+if __name__ == "__main__":
+    main()
 
 # Ejemplo de uso
-logging.info("La aplicación ha iniciado")
-logging.warning("Esto es una advertencia")
-logging.error("Esto es un error")
-
-
-try:
-    findspark.init()
-
-    from pyspark.sql import SparkSession
-    spark = SparkSession.builder \
-        .master("local[*]") \
-        .appName("ejercicio3") \
-        .getOrCreate()
-    print("✅ ¡Clúster de Spark virtual iniciado con éxito!")
-
-    print(f"Versión de Spark: {spark.version}")
-
-except Exception as e:
-    logging.error(f"Error al iniciar Spark: {e}")
-    print("❌ No se pudo iniciar el clúster de Spark virtual. Revisa los logs para más detalles.")
+# logging.info("La aplicación ha iniciado")
+# logging.warning("Esto es una advertencia")
+# logging.error("Esto es un error")
 
